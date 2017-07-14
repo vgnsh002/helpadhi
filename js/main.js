4258aeec20778641b777d38c1007d08a70749267
Vignesh <vgnsh002@gmail.com>
2017-07-14 17:18:39 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppComp from './App.js';
+import DisplayComp from './Display.js';
+
+
+ReactDOM.render(<AppComp/>, document.getElementById('app'))
+ReactDOM.render(<AppComp/>, document.getElementById('app1'))
+ReactDOM.render(<AppComp/>, document.getElementById('app2'))
+
+
+
